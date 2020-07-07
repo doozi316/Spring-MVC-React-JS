2f5e49ed5fc03d2a954813b57f2d7f988d1eb8d2
김현진 <doozi316@gmail.com>
2020-07-07 22:32:06 +0900

    오라클 연동 테스트 (mybatis X)

---
+++ src/main/java/OracleConnection.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.Test;
+
+public class OracleConnection {
+    private static final String DRIVER ="oracle.jdbc.driver.OracleDriver";
+    private static final String URL ="jdbc:oracle:thin:@127.0.0.1:1521:hjk";
+    private static final String USER ="hjk";
+    private static final String PW ="8883";
+
+    @Test
+    public void testConnect() throws Exception{
+
+        Class.forName(DRIVER);
+
+        try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+
+            System.out.println(con);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
